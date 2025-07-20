de32c8bd97c0fcbb9f7d1d19e1adf236f4d5710c
Calewiel <34463226+Calewiel@users.noreply.github.com>
2025-07-20 01:32:07 -0700

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,6 @@ import { useEffect } from 'react';
 import Head from 'next/head';
 import styles from '../styles/Resume.module.css';
 import AnimatedBackground from '../components/AnimatedBackground';
-import ImpactAvatarIcon from '../components/ImpactAvatarIcon';
 
 export default function Resume() {
   useEffect(() => {
@@ -121,17 +120,6 @@ export default function Resume() {
     fill="none"
   />
 </svg>
-
-{/* Animated Icon in Center */}
-  <div style={{
-    position: 'absolute',
-    top: '50%',
-    left: '50%',
-    transform: 'translate(-50%, -50%)',
-    zIndex: 50
-  }}>
-    <ImpactAvatarIcon />
-  </div>
           
           {/* Education Node */}
           <div className={`${styles.careerNode} ${styles.educationNode}`}>
